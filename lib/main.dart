81595f9c7d1fde60acd4e5f0be95b8bf6778ca2c
alex <alex98criado@gmail.com>
2020-02-07 15:00:12 +0100

    alex_id app creada

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  home: AlexCard()
+));
+
+
+class AlexCard extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context){
+
+  }
+
+}
+
