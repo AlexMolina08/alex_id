fe0e33d88e4d76fc16100f0aab6bb19fbd4364bd
alex <alex98criado@gmail.com>
2020-02-07 15:42:48 +0100

    App bar y datos del nombre creados

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,55 @@ class AlexCard extends StatelessWidget{
 
   @override
   Widget build(BuildContext context){
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      appBar: AppBar(
+        title: Text(
+          "Carta de Presentación" ,
+          style:TextStyle(color: Colors.red[400] , fontWeight: FontWeight.bold),
+
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.grey[400],
+        elevation: 0
+      ),
+
+      //Limitamos con un Padding los limites de la columna
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+        //Creamos la columna (Va a ser el Layout de la app)
+        child:Column(
+
+          crossAxisAlignment: CrossAxisAlignment.start,
+
+          children: <Widget>[
+            Text(
+              'NOMBRE',
+              style: TextStyle(
+                color: Colors.red[800 ],
+                letterSpacing: 1.2,
+                fontSize: 15.0,
+              )
+            ),
+
+
+            SizedBox(height: 10), //Este widget sirve para añadir espacio entre los otros
+
+            Text(
+                'Alex Molina',
+                style: TextStyle(
+                    color: Colors.black87,
+                    letterSpacing: 1.2,
+                    fontSize: 28.0,
+                    fontWeight: FontWeight.bold
+                )
+            ),
+
+          ]
+
+        )
+      )
+    );
 
   }
 
